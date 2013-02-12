2beed3d7bbbecd34b165245c358fd29b972435ae
Russ Olsen <russolsen@thinkrelevance.com>
2013-02-12 10:42:53 -0500

    Added some comments

--- spec/new_commands.rb
+++ spec/new_commands.rb
@@ -1,3 +1,7 @@
+# This is an example of what you need to do to
+# Add a new command to Vacuum: Just add a new
+# module level method to the Vacuum::Commands
+# module and off you go.
 module Vacuum
   module Commands
     def self.inc_up(path)
