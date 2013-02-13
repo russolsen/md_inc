ab987baae74facc2d6fc6383dadc316ced91616b
Russ Olsen <russolsen@thinkrelevance.com>
2013-02-13 17:54:03 -0500

    General cleanup

--- lib/md_inc.rb
+++ lib/md_inc.rb
@@ -3,30 +3,16 @@ require 'md_inc/md_inc_commands'
 
 module MdInc
   class TextProcessor
-    def process_stream(s)
-      process(s.read)
+    def process(string)
+      Commands.process(string)
+    end
+
+    def process_stream(stream)
+      Commands.process(stream.read)
     end
 
     def process_file(path)
-      process(File.read(path))
-    end
-
-    def process(content)
-      output = []
-      content.split("\n").each do |line|
-        if /^\./ =~ line
-          output << process_command(line)
-        else
-          output << line
-        end
-      end
-      out = output.flatten.join("\n")
-      out
-    end
-
-    def process_command(command_line)
-      cmd = command_line[1..-1]
-      Commands.instance_eval(cmd)
+      Commands.process(File.read(path))
     end
   end
 end
